5dbddb0db3cdc59f904780974efa14ec8eccf59d
Ed <ed@polarity.io>
2018-08-16 11:50:06 -0400

    Set default logging to info and rejectUnauthorized to true

--- config/config.js
+++ config/config.js
@@ -79,7 +79,7 @@ module.exports = {
          * to STAXX servers without valid SSL certificates.  Please note that we do NOT recommending setting this
          * to false in a production environment.
          */
-        rejectUnauthorized: false
+        rejectUnauthorized: true
     },
     logging: {
         // directory is relative to the this integrations directory
@@ -89,7 +89,7 @@ module.exports = {
         // You can also set an absolute path.  If you set an absolute path you must ensure that
         // the directory you specify is writable by the `polarityd:polarityd` user and group.
         //directoryPath: '/var/log/polarity-integrations',
-        level: 'debug',  //trace, debug, info, warn, error, fatal
+        level: 'info',  //trace, debug, info, warn, error, fatal
     },
     /**
      * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
