e738a4d28631987f4b8724a13c3c44879f8f67ae
Ed <ed@polarity.io>
2019-08-03 15:24:16 -0400

    Add hash entity type

--- config/config.js
+++ config/config.js
@@ -23,7 +23,7 @@ module.exports = {
    * @optional
    */
   description: 'Swimlane is a security automation, orchestration and incident response platform',
-  entityTypes: ['ip', 'email'],
+  entityTypes: ['ip', 'email', 'hash'],
   /**
    * An array of style files (css or less) that will be included for your integration. Any styles specified in
    * the below files can be used in your custom template.
